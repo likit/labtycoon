926b03295d12a9ba3e4f0401fe99338da1588174
Likit Preeyanon <likit.pre@mahidol.edu>
2020-03-29 15:02:39 +0700

    added pages for listing tests and choice sets

--- app/lab/views.py
+++ app/lab/views.py
@@ -1,15 +1,25 @@
 from . import lab_blueprint as lab
 from flask import render_template, url_for
+from flask_login import login_required
 from app.main.models import Laboratory
 
 
 @lab.route('/<int:lab_id>')
+@login_required
 def landing(lab_id):
     lab = Laboratory.query.get(lab_id)
     return render_template('lab/index.html', lab=lab)
 
 
 @lab.route('/<int:lab_id>/tests')
+@login_required
 def list_tests(lab_id):
     lab = Laboratory.query.get(lab_id)
     return render_template('lab/test_list.html', lab=lab)
+
+
+@lab.route('/<int:lab_id>/choice_sets')
+@login_required
+def list_choice_sets(lab_id):
+    lab = Laboratory.query.get(lab_id)
+    return render_template('lab/choice_set_list.html', lab=lab)
