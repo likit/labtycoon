659558d97d3e930219fad276d3b98e2fee0a6cd2
Likit Preeyanon <likit.pre@mahidol.edu>
2020-03-29 18:41:16 +0700

    added a new test form
    added a view to process a form data
    
    updated the link button to add new test

--- app/lab/forms.py
+++ app/lab/forms.py
@@ -1,6 +1,20 @@
-from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField
-from wtforms.validators import InputRequired
+from flask_wtf import FlaskForm, Form
+from wtforms import BooleanField, StringField, TextField, DecimalField, SelectField
+from wtforms.fields import BooleanField
+from wtforms.widgets import Select
+from wtforms.validators import InputRequired, Optional
+from wtforms_alchemy import model_form_factory, QuerySelectField
+from wtforms_alchemy.fields import QuerySelectField
+
+from .models import LabQuanTest, LabResultChoiceSet
+from app import db
+
+BaseModelForm = model_form_factory(FlaskForm)
+
+class ModelForm(BaseModelForm):
+    @classmethod
+    def get_session(self):
+        return db.session
 
 
 class ChoiceSetForm(FlaskForm):
@@ -18,3 +32,18 @@ class ChoiceItemForm(FlaskForm):
         InputRequired()
     ])
     ref = BooleanField('Is this a reference value?')
+
+
+class LabQuanTestForm(FlaskForm):
+    name = StringField('Name', validators=[
+        InputRequired()
+    ])
+    detail = TextField('Detail')
+    min_value = DecimalField('Min Value', default=0.0)
+    max_value = DecimalField('Max Value', validators=[Optional()])
+    min_ref_value = DecimalField('Min Reference Value', validators=[Optional()])
+    max_ref_value = DecimalField('Max Reference Value', validators=[Optional()])
+    choice_sets = SelectField('Choice Sets', coerce=int)
+    active = BooleanField('Active', default='checked')
+
+
--- app/lab/views.py
+++ app/lab/views.py
@@ -4,8 +4,8 @@ from flask_login import login_required, current_user
 from app.main.models import Laboratory
 from app import db
 from . import lab_blueprint as lab
-from .forms import ChoiceItemForm, ChoiceSetForm
-from .models import LabResultChoiceItem, LabActivity, LabResultChoiceSet
+from .forms import ChoiceItemForm, ChoiceSetForm, LabQuanTestForm
+from .models import LabResultChoiceItem, LabActivity, LabResultChoiceSet, LabQuanTest
 
 
 @lab.route('/<int:lab_id>')
@@ -112,3 +112,51 @@ def remove_choice_set(lab_id, choice_set_id):
     else:
         flash('The choice set does not exist.', 'warning')
     return redirect(url_for('lab.list_choice_sets', lab_id=lab_id))
+
+
+@lab.route('/<int:lab_id>/quantests/add', methods=['GET', 'POST'])
+@login_required
+def add_quan_test(lab_id):
+    form = LabQuanTestForm(lab_id=lab_id)
+    choice_sets = [(c.id, c.name) for c in LabResultChoiceSet.query.filter_by(lab_id=lab_id)]
+    form.choice_sets.choices = choice_sets
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            name = request.form.get('name')
+            detail = request.form.get('detail')
+            min_value = request.form.get('min_value')
+            max_value = request.form.get('max_value')
+            min_ref_value = request.form.get('min_ref_value')
+            max_ref_value = request.form.get('max_ref_value')
+            active = request.form.get('active')
+            min_value = float(min_value) if min_value else None
+            max_value = float(max_value) if max_value else None
+            min_ref_value = float(min_ref_value) if min_ref_value else None
+            max_ref_value = float(max_ref_value) if max_ref_value else None
+            active = True if active else False
+            new_test = LabQuanTest(
+                lab_id=lab_id,
+                name=name,
+                detail=detail,
+                min_value=min_value,
+                max_value=max_value,
+                min_ref_value=min_ref_value,
+                max_ref_value=max_ref_value,
+                active=active,
+                added_at=arrow.now('Asia/Bangkok').datetime
+            )
+            db.session.add(new_test)
+            activity = LabActivity(
+                lab_id=lab_id,
+                actor=current_user,
+                message='Added a new test',
+                detail=name,
+                added_at=arrow.now('Asia/Bangkok').datetime,
+            )
+            db.session.add(activity)
+            db.session.commit()
+            flash('New test has been added.')
+            return redirect(url_for('lab.list_tests', lab_id=lab_id))
+        else:
+            flash(form.errors, 'danger')
+    return render_template('lab/new_quan_test.html', form=form)
