18b3e8ae42d3c73463d46f4006d5a5fe4c590451
Likit Preeyanon <likit.pre@mahidol.edu>
2020-03-29 10:38:36 +0700

    added User model to the admin view

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
-from app import create_app
+from app import create_app, db, admin
 from dotenv import load_dotenv
+from flask_admin.contrib.sqla import ModelView
 
 
 load_dotenv()
@@ -7,3 +8,4 @@ load_dotenv()
 app = create_app()
 
 from app.auth.models import *
+admin.add_view(ModelView(User, db.session, category='Users'))
