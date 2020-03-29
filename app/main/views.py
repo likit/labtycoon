fd17b4e6eef4441b61c4b615e9a6be39b92fc6fc
Likit Preeyanon <likit.pre@mahidol.edu>
2020-03-29 12:53:07 +0700

    added views & pages for listing and approving members
    added humanize template filter

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,61 @@
-from flask import render_template, url_for
+import arrow
+from flask import render_template, url_for, flash, redirect
+from flask_login import login_required, current_user
 from . import main_blueprint as main
-from app.main.models import Laboratory
+from app.main.models import Laboratory, UserLabAffil
+from app import db
 
 
 @main.route('/')
+@login_required
 def index():
     labs = Laboratory.query.all()
     return render_template('main/index.html', labs=labs)
+
+
+@main.route('/labs/<int:lab_id>/members')
+@login_required
+def list_members(lab_id):
+    lab = Laboratory.query.get(lab_id)
+    return render_template('main/lab_members.html', lab=lab)
+
+
+@main.route('/labs/<int:lab_id>/join')
+@login_required
+def request_join_lab(lab_id):
+    existing_affil_record = UserLabAffil.query.filter_by(lab_id=lab_id, user_id=current_user.id).first()
+    lab = Laboratory.query.get(lab_id)
+    if existing_affil_record:
+        if not existing_affil_record.approved:
+            return 'you are already sent a request.'
+        else:
+            return 'you are already affiliated with this lab.'
+    else:
+        affil_record = UserLabAffil(
+            user=current_user,
+            lab=lab,
+            joined_at=arrow.now('Asia/Bangkok').datetime
+        )
+        db.session.add(affil_record)
+        db.session.commit()
+        return render_template('main/lab_members.html', lab=lab)
+
+
+@main.route('/labs/<int:lab_id>/approve/user/<int:user_id>')
+@login_required
+def approve_member(lab_id, user_id):
+    affil_record = UserLabAffil.query.filter_by(user_id=user_id,
+                                                lab_id=lab_id
+                                                ).first()
+    lab = Laboratory.query.get(lab_id)
+    if current_user == lab.creator:
+        if not affil_record.approved:
+            affil_record.approved = True
+            db.session.add(affil_record)
+            db.session.commit()
+            flash('The user has been approved to join the lab.', 'success')
+        else:
+            flash('The user has already been approved.')
+    else:
+        flash('You do not have a permission to approve a user.', 'danger')
+    return redirect(url_for('main.list_members', lab_id=lab_id))
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from app import create_app, db, admin
 from dotenv import load_dotenv
 from flask_admin.contrib.sqla import ModelView
+import arrow
 
 
 load_dotenv()
@@ -12,3 +13,9 @@ admin.add_view(ModelView(User, db.session, category='Users'))
 
 from app.main.models import *
 admin.add_view(ModelView(Laboratory, db.session, category='Labs'))
+
+
+@app.template_filter('humanizedt')
+def humanize_datetime(dt):
+    dt = arrow.get(dt)
+    return dt.humanize()
