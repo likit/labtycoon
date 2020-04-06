7cb9dbc55e4d51971f49add9846428f92bbd3866
Likit Preeyanon <likit.pre@mahidol.edu>
2020-04-06 23:55:39 +0700

    added lab affiliation model to the admin view

--- wsgi.py
+++ wsgi.py
@@ -25,6 +25,7 @@ admin.add_view(ModelView(LabResultChoiceItem, db.session, category='Tests'))
 admin.add_view(ModelView(LabActivity, db.session, category='Activities'))
 admin.add_view(ModelView(LabCustomer, db.session, category='Customers'))
 admin.add_view(ModelView(Announcement, db.session, category='Announcement'))
+admin.add_view(ModelView(UserLabAffil, db.session, category='Labs'))
 
 
 @app.route('/')
